d86eeadd59f055767283fe57f751db62648275d1
JayGangkun <jaygangkun@hotmail.com>
2021-11-26 02:37:47 -0500

    draw polygons

--- src/Components/WorkStage.js
+++ src/Components/WorkStage.js
@@ -1,5 +1,5 @@
-
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { CRow, CCol, CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem, CDropdownDivider, CCard, CCardHeader, CCardBody } from '@coreui/react'
 import CIcon from '@coreui/icons-react';
 import * as icon from '@coreui/icons';
@@ -9,18 +9,104 @@ import HtmlToReact from 'html-to-react';
 
 const HtmlToReactParser = new HtmlToReact.Parser();
 
+const isValidNode = function () {
+    return true;
+};
+
+const preprocessingInstructions = [
+    {
+        shouldPreprocessNode: function (node) {
+            return node.attribs && node.attribs['data-process'] === 'shared';
+        },
+        preprocessNode: function (node) {
+            node.attribs = {id: `preprocessed-${node.attribs.id}`,};
+        },
+    }
+];
+const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
+const processingInstructions = [
+    // {
+    //     shouldProcessNode: function (node) {
+    //         return node.attribs && node.attribs.id === 'preprocessed-first';
+    //     },
+    //     processNode: function(node, children, index) {
+    //         return React.createElement('h1', {key: index, id: node.attribs.id,}, 'First');
+    //     },
+    // },
+    {
+        shouldProcessNode: function (node) {
+            return node.tagName === 'svg';
+        },
+        processNode: function (node, children, index) {
+            node.attribs.className = 'svgUpload';
+            node.attribs.id = 'svgUpload';
+            return processNodeDefinitions.processDefaultNode(node, children, index);
+        },
+    },
+    {
+        shouldProcessNode: function (node) {
+            return true;
+        },
+        processNode: processNodeDefinitions.processDefaultNode,
+    },
+];
+
+function UploadSvg(svgText) {
+    return (
+        <div className="UploadSvg">
+            { HtmlToReactParser.parseWithInstructions(svgText, isValidNode, processingInstructions, preprocessingInstructions) }
+        </div>
+    )
+}
+
+function DrawSvg(constraints, constraintPoints, eventPoints) {
+    console.log('DrawSvg >>', eventPoints);
+    return (
+        <div className="DrawSvg">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="243 70 2707 4313">
+                {
+                    constraints?.map((constraint, index) => {
+                        return (
+                            <polygon key={index} className="polyline polyline_{index}" opacity="0.3" fill="blue" points={constraint?.points?.toString()}></polygon>
+                        )
+                    })
+                }
+                <polygon className="polyline polyline_{index}" opacity="0.3" fill="blue" points={constraintPoints?.toString()}></polygon>
+            </svg>
+            <div className="DrawSvgPoints">
+            {
+                eventPoints?.map((point, index) => {
+                    return (
+                        <span key={index} className="DrawSvgPoint" style={{left: point.x, top: point.y}}/>
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
+}
+
+function WorkToolBarButton(props) {
+    const className = props.active ? "btn btn-sm btn-active mx-1 rounded" : "btn btn-sm btn-secondary mx-1 rounded disabled";
+    return (
+        <span className={className} onClick={props.onClick}>
+            {props.children}
+        </span>
+    )
+}
+
 function WorkStage() {
     const fileSelector = document.createElement('input');
     fileSelector.setAttribute('type', 'file');
     fileSelector.setAttribute('accept', '.svg');
-    const [svgRender, setSVGRender] = React.useState('');
+    // const [svgElement, setSvgElement] = React.useState('');
+    const [ svgFile, setSvgFile ] = React.useState('');
     const readFile = function(evt) {
         var reader  = new FileReader();
         reader.readAsText(fileSelector.files[0]);
         reader.onloadend = function () {
-                    
-            console.log('file read>>>>', reader.result);
-            setSVGRender(reader.result);
+            setSvgFile(reader.result);
+            setBtnEditActive(true);
         }
     }
 
@@ -29,68 +115,24 @@ function WorkStage() {
         fileSelector.click();
     }
 
-    const isValidNode = function () {
-        return true;
-    };
-    
-    const preprocessingInstructions = [
-        {
-            shouldPreprocessNode: function (node) {
-                return node.attribs && node.attribs['data-process'] === 'shared';
-            },
-            preprocessNode: function (node) {
-                node.attribs = {id: `preprocessed-${node.attribs.id}`,};
-            },
-        }
-    ];
-    const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
-    const processingInstructions = [
-        // {
-        //     shouldProcessNode: function (node) {
-        //         return node.attribs && node.attribs.id === 'preprocessed-first';
-        //     },
-        //     processNode: function(node, children, index) {
-        //         return React.createElement('h1', {key: index, id: node.attribs.id,}, 'First');
-        //     },
-        // },
-        // {
-        //     shouldProcessNode: function (node) {
-        //         return node.attribs && node.attribs.id === 'preprocessed-second';
-        //     },
-        //     processNode: function (node, children, index) {
-        //         return React.createElement('h2', {key: index, id: node.attribs.id,}, 'Second');
-        //     },
-        // },
-        {
-            shouldProcessNode: function (node) {
-                return node.tagName === 'svg';
-            },
-            processNode: function (node, children, index) {
-                node.attribs.className = 'svgUpload';
-                return processNodeDefinitions.processDefaultNode(node, children, index);
-            },
-        },
-        {
-            shouldProcessNode: function (node) {
-                return true;
-            },
-            processNode: processNodeDefinitions.processDefaultNode,
-        },
-    ];
+    const [isEdit, setIsEdit] = React.useState(false);
 
-    let constraints = [];
-    let constraint_points = [];
-    const [changes, setChanges] = React.useState(false);
+    const [constraints, setContraints] = React.useState([]);
+    const [constraintPoints, setConstraintPoints] = React.useState([]);
+    const [eventPoints, setEventPoints] = React.useState([]);
     
     const stageClick = function(event) {
-
-        // if(!isEdit) {
-        //     return;
-        // }
-
+        console.log('event:', event);
+        const stageWrapContent = document.getElementById('stageWrapContent');
         const svg = document.getElementsByClassName('svgUpload')[0];
+        
+        if(typeof svg == 'undefined' || svg == null) {    
+            alert('No SVG Uploaded!');
+            return;
+        }
 
-        if(typeof svg == 'undefined' || svg == null) {
+        if(!isEdit) {
+            alert('Please Click Constraint Icon');
             return;
         }
 
@@ -106,47 +148,28 @@ function WorkStage() {
         
             // transform to SVG coordinates
             const svgP = pt.matrixTransform(svg.getScreenCTM().inverse());
-            // polyline1.push(svgP.x);
-            // polyline1.push(svgP.y);
-            let polygon;
-            if(constraint_points.length == 0) {
-                polygon = document.createElement('polygon');
-                polygon.className="polyline polyline_";
-                polygon.setAttribute('opacity', 0.3);
-                polygon.setAttribute('fill', 'blue');
-                svg.appendChild(polygon);
-            }
-            else {
-                polygon = svg.getElementsByTagName('polygon')[0];
-            }
 
-            if(typeof polygon == 'undefined') {
-                return;
-            }
+            setConstraintPoints([...constraintPoints, [svgP.x, svgP.y]]);
 
-            constraint_points.push(svgP.x);
-            constraint_points.push(svgP.y);
-            polygon.setAttribute('points', constraint_points.toString());
-            
-            // $('.polyline1_' + count_contraints_poly_1).attr('points', polyline1.toString());
-
-            // coordinates_svg1.push([
-            //     svgP.x,
-            //     svgP.y
-            // ]);
-            if(constraint_points.length > 6) {
-                setChanges(!changes);
-            }
-            
+            setEventPoints([...eventPoints, {x: event.pageX - stageWrapContent.getBoundingClientRect().left, y: event.pageY - stageWrapContent.getBoundingClientRect().top}]);
         }
     }
 
-    const [isEdit, setIsEdit] = React.useState(false);
-    const [svgLoaded, setSvgLoaded] = React.useState(false);
-
     const cbEditBtnClick = function() {
+        if(isEdit) {
+            setContraints([...constraints, {points: constraintPoints}])
+            setConstraintPoints([]);
+            setEventPoints([]);
+        }
         setIsEdit(!isEdit);
     }
+
+    const [btnOpenActive, setBtnOpenActive] = React.useState(true);
+    const [btnEditActive, setBtnEditActive] = React.useState(false);
+    const [btnZoomInActive, setBtnZoomInActive] = React.useState(false);
+    const [btnZoomOutActive, setBtnZoomOutActive] = React.useState(false);
+    const [btnHandActive, setBtnHandActive] = React.useState(false);
+    const [btnResetActive, setBtnResetActive] = React.useState(false);
 
     return (
         <CRow>
@@ -156,12 +179,12 @@ function WorkStage() {
                         <div className="d-flex align-items-center justify-content-between">
                             <span>Canvas</span>
                             <div className="">
-                                <span className="btn btn-sm btn-secondary mx-1 rounded" onClick={() => openFile()}><FontAwesomeIcon icon={faFolderOpen} /></span>
-                                <span className="btn btn-sm btn-secondary mx-1 rounded" onClick={() => cbEditBtnClick()}><FontAwesomeIcon icon={isEdit ? faCheck : faCogs} /></span>
-                                <span className="btn btn-sm btn-secondary mx-1 rounded"><FontAwesomeIcon icon={faSearchPlus} /></span>
-                                <span className="btn btn-sm btn-secondary mx-1 rounded"><FontAwesomeIcon icon={faSearchMinus} /></span>
-                                <span className="btn btn-sm btn-secondary mx-1 rounded"><FontAwesomeIcon icon={faHandPaper} /></span>
-                                <span className="btn btn-sm btn-secondary mx-1 rounded"><FontAwesomeIcon icon={faUndo} /></span>
+                                <WorkToolBarButton active={btnOpenActive} onClick={() => openFile()}><FontAwesomeIcon icon={faFolderOpen} /></WorkToolBarButton>
+                                <WorkToolBarButton active={btnEditActive} onClick={() => cbEditBtnClick()}><FontAwesomeIcon icon={isEdit ? faCheck : faCogs} /></WorkToolBarButton>
+                                <WorkToolBarButton active={btnZoomInActive} ><FontAwesomeIcon icon={faSearchPlus} /></WorkToolBarButton>
+                                <WorkToolBarButton active={btnZoomOutActive} ><FontAwesomeIcon icon={faSearchMinus} /></WorkToolBarButton>
+                                <WorkToolBarButton active={btnHandActive} ><FontAwesomeIcon icon={faHandPaper} /></WorkToolBarButton>
+                                <WorkToolBarButton active={btnResetActive} ><FontAwesomeIcon icon={faUndo} /></WorkToolBarButton>
                             </div>
                             <CDropdown>
                                 <CDropdownToggle className="no-direction" color="" size="sm"><CIcon icon={icon.cilOptions} /></CDropdownToggle>
@@ -187,9 +210,11 @@ function WorkStage() {
                     </CCardHeader>
                     <CCardBody>
                         <div className="stageContainer">
-                            {changes ? "Change" : "NoChange"}
-                            <div className="stageWrap" onClick={(e) => stageClick(e)}>
-                                { HtmlToReactParser.parseWithInstructions(svgRender, isValidNode, processingInstructions, preprocessingInstructions) }
+                            <div className="stageWrap" id="stageWrap">
+                                <div className="stageWrapContent" id="stageWrapContent" onClick={(e) => stageClick(e)}>
+                                    { UploadSvg(svgFile) }
+                                    { DrawSvg(constraints, constraintPoints, eventPoints) }
+                                </div>
                             </div>
                         </div>
                     </CCardBody>
