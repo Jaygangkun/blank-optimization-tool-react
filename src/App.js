63da505d2f6cab9466662ee5c5e02b2a79ef1bd8
JayGangkun <jaygangkun@hotmail.com>
2021-11-28 20:31:47 -0500

    hand, reset tool button done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-
 import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 import React from 'react'
-import { CHeader, CContainer, CHeaderBrand, CHeaderToggler, CCollapse, CHeaderNav, CNavItem, CNavLink, CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem, CDropdownDivider, CForm, CFormInput, CButton, CSidebar, CSidebarBrand, CSidebarNav, CNavTitle, CBadge, CNavGroup, CSidebarToggler, CImage, CCard, CCardBody, CCardTitle} from '@coreui/react'
-import CIcon from '@coreui/icons-react';
-import * as icon from '@coreui/icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCogs, faSearchPlus, faSearchMinus, faHandPaper, faUndo, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+import { CContainer } from '@coreui/react'
 import AppSidebar from './Components/AppSidebar';
 import AppHeader from './Components/AppHeader';
 import WorkStage from './Components/WorkStage';
 
 function App() {
-  const [visible, setVisible] = React.useState(false)
   return (
     <div className="App">
       <AppSidebar />
--- src/Components/WorkStage.js
+++ src/Components/WorkStage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import { CRow, CCol, CDropdown, CDropdownToggle, CDropdownMenu, CDropdownItem, CDropdownDivider, CCard, CCardHeader, CCardBody } from '@coreui/react'
 import CIcon from '@coreui/icons-react';
 import * as icon from '@coreui/icons';
@@ -8,6 +7,17 @@ import { faCogs, faSearchPlus, faSearchMinus, faHandPaper, faUndo, faFolderOpen,
 import HtmlToReact from 'html-to-react';
 
 const HtmlToReactParser = new HtmlToReact.Parser();
+
+let svgTagAttr = {};
+let svgUpload = null;
+let svgDraw = null;
+let stageWrapContent = null;
+
+let dragImg = null;
+
+let svgRectViewInitial = null;
+let svgRectViewDragStart = null;
+let dragStartPoint = null;
 
 const isValidNode = function () {
     return true;
@@ -23,6 +33,7 @@ const preprocessingInstructions = [
         },
     }
 ];
+
 const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
 const processingInstructions = [
     // {
@@ -38,8 +49,10 @@ const processingInstructions = [
             return node.tagName === 'svg';
         },
         processNode: function (node, children, index) {
+            svgTagAttr = node.attribs;
+
             node.attribs.className = 'svgUpload';
-            node.attribs.id = 'svgUpload';
+            node.attribs.id = 'svgUpload';            
             return processNodeDefinitions.processDefaultNode(node, children, index);
         },
     },
@@ -52,6 +65,34 @@ const processingInstructions = [
 ];
 
 function UploadSvg(svgText) {
+    React.useEffect(() => {
+        if(svgUpload == null) {
+            svgUpload = document.getElementById('svgUpload');
+        }
+
+        if(svgDraw == null) {
+            svgDraw = document.getElementById('svgDraw');
+        }   
+
+        if(stageWrapContent == null) {
+            stageWrapContent = document.getElementById('stageWrapContent');
+        }
+
+        if(dragImg == null) {
+            dragImg = document.createElement("img");
+        }
+
+        if(svgRectViewInitial == null && svgUpload != null) {
+            const svgRectViewBox = svgUpload.getAttribute('viewBox').split(' ');
+        
+            svgRectViewInitial = {
+                x: svgRectViewBox[0],
+                y: svgRectViewBox[1],
+                width: svgRectViewBox[2],
+                height: svgRectViewBox[3],
+            }
+        }
+    });
     return (
         <div className="UploadSvg">
             { HtmlToReactParser.parseWithInstructions(svgText, isValidNode, processingInstructions, preprocessingInstructions) }
@@ -60,10 +101,9 @@ function UploadSvg(svgText) {
 }
 
 function DrawSvg(constraints, constraintPoints, eventPoints) {
-    console.log('DrawSvg >>', eventPoints);
     return (
         <div className="DrawSvg">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="243 70 2707 4313">
+            <svg xmlns={svgTagAttr.xmlns} viewBox={svgTagAttr.viewbox} id="svgDraw">
                 {
                     constraints?.map((constraint, index) => {
                         return (
@@ -87,12 +127,92 @@ function DrawSvg(constraints, constraintPoints, eventPoints) {
 }
 
 function WorkToolBarButton(props) {
-    const className = props.active ? "btn btn-sm btn-active mx-1 rounded" : "btn btn-sm btn-secondary mx-1 rounded disabled";
+    const className = props.active ? "btn btn-sm btn-active mx-1 rounded" : (props.enable ? "btn btn-sm btn-enable mx-1 rounded" : "btn btn-sm btn-secondary mx-1 rounded disabled");
     return (
         <span className={className} onClick={props.onClick}>
             {props.children}
         </span>
     )
+}
+
+const zoom = (direction) => {
+
+    if(svgUpload == null) {
+        return;
+    }
+
+    if(svgDraw == null) {
+        return;
+    }
+
+    if(direction === 'out') {
+        svgUpload.setAttribute(
+            'viewBox',
+            svgUpload.getAttribute('viewBox').split(' ')[0] + ' ' + 
+            svgUpload.getAttribute('viewBox').split(' ')[1] + ' ' + 
+            (Number(svgUpload.getAttribute('viewBox').split(' ')[2]) + Number(svgUpload.getAttribute('viewBox').split(' ')[2]*0.1)) + ' ' + 
+            (Number(svgUpload.getAttribute('viewBox').split(' ')[3]) + Number(svgUpload.getAttribute('viewBox').split(' ')[3]*0.1))
+        );
+
+        svgDraw.setAttribute(
+            'viewBox',
+            svgDraw.getAttribute('viewBox').split(' ')[0] + ' ' + 
+            svgDraw.getAttribute('viewBox').split(' ')[1] + ' ' + 
+            (Number(svgDraw.getAttribute('viewBox').split(' ')[2]) + Number(svgDraw.getAttribute('viewBox').split(' ')[2]*0.1)) + ' ' + 
+            (Number(svgDraw.getAttribute('viewBox').split(' ')[3]) + Number(svgDraw.getAttribute('viewBox').split(' ')[3]*0.1))
+        );
+    }
+    else if (direction === "in")
+    {
+        svgUpload.setAttribute(
+            'viewBox',
+            svgUpload.getAttribute('viewBox').split(' ')[0] + ' ' + 
+            svgUpload.getAttribute('viewBox').split(' ')[1] + ' ' + 
+            (Number(svgUpload.getAttribute('viewBox').split(' ')[2]) - Number(svgUpload.getAttribute('viewBox').split(' ')[2]*0.1)) + ' ' + 
+            (Number(svgUpload.getAttribute('viewBox').split(' ')[3]) - Number(svgUpload.getAttribute('viewBox').split(' ')[3]*0.1))
+        );
+
+        svgDraw.setAttribute(
+            'viewBox',
+            svgDraw.getAttribute('viewBox').split(' ')[0] + ' ' + 
+            svgDraw.getAttribute('viewBox').split(' ')[1] + ' ' + 
+            (Number(svgDraw.getAttribute('viewBox').split(' ')[2]) - Number(svgDraw.getAttribute('viewBox').split(' ')[2]*0.1)) + ' ' + 
+            (Number(svgDraw.getAttribute('viewBox').split(' ')[3]) - Number(svgDraw.getAttribute('viewBox').split(' ')[3]*0.1))
+        );
+    }
+}
+
+const panMove = (offset) => {
+    if(svgUpload == null) {
+        return;
+    }
+
+    if(svgDraw == null) {
+        return;
+    }
+
+    // convert view offset to svg offset
+    const svgDomRect = svgUpload.getBoundingClientRect();
+    const viewBoxOffset = {
+        x: offset.x * svgRectViewDragStart.width / svgDomRect.width,
+        y: offset.y * svgRectViewDragStart.height / svgDomRect.height
+    }
+    
+    svgUpload.setAttribute(
+        'viewBox',
+        (parseInt(svgRectViewDragStart.x) + parseInt(viewBoxOffset.x)) + ' ' + 
+        (parseInt(svgRectViewDragStart.y) + parseInt(viewBoxOffset.y)) + ' ' + 
+        svgRectViewDragStart.width + ' ' + 
+        svgRectViewDragStart.height
+    );
+
+    svgDraw.setAttribute(
+        'viewBox',
+        (parseInt(svgRectViewDragStart.x) + parseInt(viewBoxOffset.x)) + ' ' + 
+        (parseInt(svgRectViewDragStart.y) + parseInt(viewBoxOffset.y)) + ' ' + 
+        svgRectViewDragStart.width + ' ' + 
+        svgRectViewDragStart.height
+    );
 }
 
 function WorkStage() {
@@ -106,7 +226,11 @@ function WorkStage() {
         reader.readAsText(fileSelector.files[0]);
         reader.onloadend = function () {
             setSvgFile(reader.result);
-            setBtnEditActive(true);
+            setBtnEditEnable(true);
+            setBtnZoomInEnable(true);
+            setBtnZoomOutEnable(true);
+            setBtnHandEnable(true);
+            setBtnResetEnable(true);
         }
     }
 
@@ -122,12 +246,16 @@ function WorkStage() {
     const [eventPoints, setEventPoints] = React.useState([]);
     
     const stageClick = function(event) {
-        console.log('event:', event);
-        const stageWrapContent = document.getElementById('stageWrapContent');
-        const svg = document.getElementsByClassName('svgUpload')[0];
-        
-        if(typeof svg == 'undefined' || svg == null) {    
+        if(svgUpload == null || svgDraw == null) {
+            return;
+        }
+
+        if(typeof svgUpload == 'undefined') {
             alert('No SVG Uploaded!');
+            return;
+        }
+
+        if(btnHandActive) {
             return;
         }
 
@@ -136,23 +264,84 @@ function WorkStage() {
             return;
         }
 
-        if(typeof svg.firstElementChild.tagName == 'g') {
+        if(typeof svgUpload.firstElementChild.tagName == 'g') {
 
         }
         else {
-            const pt = svg.createSVGPoint();    
+            const pt = svgUpload.createSVGPoint();    
                 
             // pass event coordinates
             pt.x = event.clientX;
             pt.y = event.clientY;
         
             // transform to SVG coordinates
-            const svgP = pt.matrixTransform(svg.getScreenCTM().inverse());
+            const svgP = pt.matrixTransform(svgUpload.getScreenCTM().inverse());
 
             setConstraintPoints([...constraintPoints, [svgP.x, svgP.y]]);
 
-            setEventPoints([...eventPoints, {x: event.pageX - stageWrapContent.getBoundingClientRect().left, y: event.pageY - stageWrapContent.getBoundingClientRect().top}]);
+            setEventPoints([...eventPoints, {x: event.clientX - stageWrapContent.getBoundingClientRect().left, y: event.clientY - stageWrapContent.getBoundingClientRect().top}]);
         }
+    }
+
+    
+    
+    const stageDragStart = function(event) {
+
+        if(!btnHandActive) {
+            return;
+        }
+
+        event.dataTransfer.setDragImage(dragImg, 0, 0);
+
+        const svgRectViewBox = svgUpload.getAttribute('viewBox').split(' ');
+        
+        svgRectViewDragStart = {
+            x: svgRectViewBox[0],
+            y: svgRectViewBox[1],
+            width: svgRectViewBox[2],
+            height: svgRectViewBox[3],
+        }
+
+        dragStartPoint = {
+            x: event.clientX,
+            y: event.clientY
+        };
+    }
+
+    const stageDragEnd = function(event) {
+        if(!btnHandActive) {
+            return;
+        }
+
+        svgRectViewDragStart = null;
+        dragStartPoint = null;
+    }
+
+    const stageDrag = function(event) {
+        
+        if(!btnHandActive) {
+            return;
+        }
+
+        if(event.clientX != 0 && event.clientY != 0) {
+            panMove({
+                x: dragStartPoint.x - event.clientX,
+                y: dragStartPoint.y - event.clientY
+            })
+        }
+    }
+
+    const [btnEditEnable, setBtnEditEnable] = React.useState(false);
+    const [btnZoomInEnable, setBtnZoomInEnable] = React.useState(false);
+    const [btnZoomOutEnable, setBtnZoomOutEnable] = React.useState(false);
+    const [btnHandEnable, setBtnHandEnable] = React.useState(false);
+    const [btnResetEnable, setBtnResetEnable] = React.useState(false);
+
+    const [btnEditActive, setBtnEditActive] = React.useState(false);
+    const [btnHandActive, setBtnHandActive] = React.useState(false);
+
+    const cbOpenBtnClick = function() {
+        openFile()
     }
 
     const cbEditBtnClick = function() {
@@ -160,31 +349,90 @@ function WorkStage() {
             setContraints([...constraints, {points: constraintPoints}])
             setConstraintPoints([]);
             setEventPoints([]);
+
+            setBtnZoomInEnable(true);
+            setBtnZoomOutEnable(true);
+            setBtnHandEnable(true);
+
+            setBtnEditActive(false);
         }
+        else {
+            setBtnZoomInEnable(false);
+            setBtnZoomOutEnable(false);
+            setBtnHandEnable(false);
+
+            setBtnEditActive(true);
+        }
+
         setIsEdit(!isEdit);
+
+        setBtnHandActive(false);
     }
 
-    const [btnOpenActive, setBtnOpenActive] = React.useState(true);
-    const [btnEditActive, setBtnEditActive] = React.useState(false);
-    const [btnZoomInActive, setBtnZoomInActive] = React.useState(false);
-    const [btnZoomOutActive, setBtnZoomOutActive] = React.useState(false);
-    const [btnHandActive, setBtnHandActive] = React.useState(false);
-    const [btnResetActive, setBtnResetActive] = React.useState(false);
+    const cbZoomInBtnClick = function() {
+        zoom('in')
 
+        setBtnEditActive(false);
+        setBtnHandActive(false);
+    }
+
+    const cbZoomOutBtnClick = function() {
+        zoom('out')
+
+        setBtnEditActive(false);
+        setBtnHandActive(false);
+    }
+
+    const cbHandBtnClick = function() {
+        
+        setBtnEditActive(false);
+        setBtnHandActive(true);
+    }
+
+    const cbResetBtnClick = function() {
+
+        if(svgUpload == null) {
+            return;
+        }
+    
+        if(svgDraw == null) {
+            return;
+        }
+    
+        svgUpload.setAttribute(
+            'viewBox',
+            parseInt(svgRectViewInitial.x) + ' ' + 
+            parseInt(svgRectViewInitial.y) + ' ' + 
+            svgRectViewInitial.width + ' ' + 
+            svgRectViewInitial.height
+        );
+    
+        svgDraw.setAttribute(
+            'viewBox',
+            parseInt(svgRectViewInitial.x) + ' ' + 
+            parseInt(svgRectViewInitial.y) + ' ' + 
+            svgRectViewInitial.width + ' ' + 
+            svgRectViewInitial.height
+        );
+
+        setBtnEditActive(false);
+        setBtnHandActive(false);
+    }
+    
     return (
         <CRow>
-            <CCol xs={6}>
+            <CCol xs={12}>
                 <CCard className="my-4">
                     <CCardHeader>
                         <div className="d-flex align-items-center justify-content-between">
                             <span>Canvas</span>
                             <div className="">
-                                <WorkToolBarButton active={btnOpenActive} onClick={() => openFile()}><FontAwesomeIcon icon={faFolderOpen} /></WorkToolBarButton>
-                                <WorkToolBarButton active={btnEditActive} onClick={() => cbEditBtnClick()}><FontAwesomeIcon icon={isEdit ? faCheck : faCogs} /></WorkToolBarButton>
-                                <WorkToolBarButton active={btnZoomInActive} ><FontAwesomeIcon icon={faSearchPlus} /></WorkToolBarButton>
-                                <WorkToolBarButton active={btnZoomOutActive} ><FontAwesomeIcon icon={faSearchMinus} /></WorkToolBarButton>
-                                <WorkToolBarButton active={btnHandActive} ><FontAwesomeIcon icon={faHandPaper} /></WorkToolBarButton>
-                                <WorkToolBarButton active={btnResetActive} ><FontAwesomeIcon icon={faUndo} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={true} onClick={() => cbOpenBtnClick()}><FontAwesomeIcon icon={faFolderOpen} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={btnEditEnable} active={btnEditActive} onClick={() => cbEditBtnClick()}><FontAwesomeIcon icon={isEdit ? faCheck : faCogs} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={btnZoomInEnable} onClick={() => cbZoomInBtnClick()}><FontAwesomeIcon icon={faSearchPlus} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={btnZoomOutEnable} onClick={() => cbZoomOutBtnClick()}><FontAwesomeIcon icon={faSearchMinus} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={btnHandEnable} active={btnHandActive} onClick={() => cbHandBtnClick()}><FontAwesomeIcon icon={faHandPaper} /></WorkToolBarButton>
+                                <WorkToolBarButton enable={btnResetEnable} onClick={() => cbResetBtnClick()}><FontAwesomeIcon icon={faUndo} /></WorkToolBarButton>
                             </div>
                             <CDropdown>
                                 <CDropdownToggle className="no-direction" color="" size="sm"><CIcon icon={icon.cilOptions} /></CDropdownToggle>
@@ -211,7 +459,16 @@ function WorkStage() {
                     <CCardBody>
                         <div className="stageContainer">
                             <div className="stageWrap" id="stageWrap">
-                                <div className="stageWrapContent" id="stageWrapContent" onClick={(e) => stageClick(e)}>
+                                <div 
+                                    className="stageWrapContent" 
+                                    id="stageWrapContent" 
+                                    style={{cursor: btnHandActive ? 'move' : 'initial'}}
+                                    draggable={btnHandActive}
+                                    onClick={(e) => stageClick(e)} 
+                                    onDrag={(e) => stageDrag(e)} 
+                                    onDragStart={(e) => stageDragStart(e)}
+                                    onDragEnd={(e) => stageDragEnd(e)}
+                                >
                                     { UploadSvg(svgFile) }
                                     { DrawSvg(constraints, constraintPoints, eventPoints) }
                                 </div>
